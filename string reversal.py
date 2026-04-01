a1737914b8ecd747d1dd21befe5920dcbd589b42
Nistha <nisthabhattacharjee7@gmail.com>
2026-04-01 09:58:21 +0530

    fix file name for string reversal

--- string reversal.py
+++
@@ -1,4 +0,0 @@
-str = "Nistha"
-print(str[: : -1])         
-#  in this reversal or printing by slicing , its [start:end :step]
-# and -1 means 1 step backward and will print the string in reverse order 
---
+++ string_reversal.py
@@ -0,0 +1,4 @@
+str = "Nistha"
+print(str[: : -1])         
+#  in this reversal or printing by slicing , its [start:end :step]
+# and -1 means 1 step backward and will print the string in reverse order 
