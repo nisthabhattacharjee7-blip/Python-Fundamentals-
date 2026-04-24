b112cf1317d68c5ac13f37bbc122c03b85622391
Nistha <nisthabhattacharjee7@gmail.com>
2026-04-25 02:08:29 +0530

    use if in and is

---
+++ membership_and_identity_operators.py
@@ -0,0 +1,25 @@
+l = [1, 2, 3, 4, 5]
+s = "Hello World"
+d = {1: "Nistha", 2: "Is", 3: "Devi"}
+
+print(5 in l)
+print('O' in s)
+print(4 in d)
+
+# identity operator 
+n1 = 10
+n2 = 10
+b = [1,2,3]
+c = [2,3,4]
+a = c
+
+s1 = "Hello"
+s2 = "hello"
+
+
+print(n1 is n2) #returns True
+print(a is c) #returns True
+print(b is c) #returns False
+print(s1 is s2) #returns False
+
+
--- membership_operators.py
+++
@@ -1,7 +0,0 @@
-l = [1, 2, 3, 4, 5]
-s = "Hello World"
-d = {1: "Nistha", 2: "Is", 3: "Devi"}
-
-print(5 in l)
-print('O' in s)
-print(4 in d)
